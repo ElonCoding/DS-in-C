d98e1a46bcf6bc9ff53161c6e78198084e18479b
Parikshit Sharma <sharmaparikshit405@gmail.com>
2025-11-03 11:10:21 +0530

    lect4

--- lect4/swaping-of-last-second-last-node-using-circular-method.c
+++ lect4/swaping-of-last-second-last-node-using-circular-method.c
@@ -2,67 +2,84 @@
 
 struct Node {
     int data;
+    struct Node* prev;
     struct Node* next;
 };
 
 void printList(struct Node* head) {
-    if (head == 0) {
+    if (head == NULL) {
         printf("List empty\n");
         return;
     }
     struct Node* temp = head;
     do {
-        printf("%d -> ", temp->data);
+        printf("%d <-> ", temp->data);
         temp = temp->next;
     } while (temp != head);
     printf("(back to head)\n");
 }
 
 void swapLastTwoNodes(struct Node** head) {
-    if (*head == 0 || (*head)->next == *head)
+    if (*head == NULL || (*head)->next == *head) {
+        // List is empty or has only one node, nothing to swap
         return;
-
-    struct Node* first = *head;
-    struct Node* secondLast = *head;
-    struct Node* last = (*head)->next;
-
-    while (last->next != *head) {
-        secondLast = last;
-        last = last->next;
     }
 
-    secondLast->next = *head;
-    struct Node* temp = (*head)->next;
+    struct Node* last = (*head)->prev;
+    struct Node* secondLast = last->prev;
 
-    if (temp == *head) {
+    if (secondLast == *head) { // Only two nodes in the list
+        // Swap head and last node
         *head = last;
         last->next = secondLast;
+        last->prev = secondLast;
+        secondLast->next = last;
+        secondLast->prev = last;
         return;
     }
 
-    struct Node* cur = *head;
-    while (cur->next != secondLast)
-        cur = cur->next;
+    // General case: more than two nodes
+    struct Node* thirdLast = secondLast->prev;
+    struct Node* first = *head;
 
-    cur->next = last;
+    // Adjust pointers for thirdLast and last
+    thirdLast->next = last;
+    last->prev = thirdLast;
+
+    // Adjust pointers for last and secondLast
     last->next = secondLast;
-    secondLast->next = *head;
+    secondLast->prev = last;
+
+    // Adjust pointers for secondLast and first (head)
+    secondLast->next = first;
+    first->prev = secondLast;
+
+    // If the head was one of the swapped nodes, update head
+    if (*head == secondLast) {
+        *head = last;
+    }
 }
 
 int main() {
-    int n, i;
-    printf("Enter number of nodes: ");
-    scanf("%d", &n);
+    // Create a sample doubly circular linked list
+    struct Node node1 = {1, NULL, NULL};
+    struct Node node2 = {2, NULL, NULL};
+    struct Node node3 = {3, NULL, NULL};
+    struct Node node4 = {4, NULL, NULL};
 
-    struct Node nodes[100];
+    node1.next = &node2;
+    node1.prev = &node4;
 
-    for (i = 0; i < n; i++) {
-        printf("Enter data for node %d: ", i + 1);
-        scanf("%d", &nodes[i].data);
-        nodes[i].next = &nodes[(i + 1) % n];
-    }
+    node2.next = &node3;
+    node2.prev = &node1;
 
-    struct Node* head = &nodes[0];
+    node3.next = &node4;
+    node3.prev = &node2;
+
+    node4.next = &node1;
+    node4.prev = &node3;
+
+    struct Node* head = &node1;
 
     printf("\nOriginal list:\n");
     printList(head);
